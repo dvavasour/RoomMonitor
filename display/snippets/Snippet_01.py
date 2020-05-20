52c634aa42ef6ad806c8c97ac6e2ecc8ccbf69cf
Dunstan Vavasour <dunstan@vavasour.org.uk>
2020-05-20 11:55:51 +0100

    Hello World working correctly

--- display/snippets/Snippet_01.py
+++ display/snippets/Snippet_01.py
@@ -1,20 +1,48 @@
 from inky import InkyPHAT
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
 
 
 
 
 
+def initialiseDisplay():
+    global inkyDisplay
+    inkyDisplay = InkyPHAT("red")
+    inkyDisplay.set_border(inkyDisplay.WHITE)
 
-def initialise ():
-    inky_display = InkyPHAT("red")
-    inky_display.set_border(inky_display.WHITE)
+def initialiseImage():
+    global img, draw
+    img = Image.new("P", (inkyDisplay.WIDTH, inkyDisplay.HEIGHT))
+    draw = ImageDraw.Draw(img)
 
-def default ():
+def initialiseFont():
+    global font
+    font = ImageFont.truetype(FredokaOne, 22)
+
+
+def HelloWorld():
+    print("In HelloWorld function")
+    message = "Hello, World!"
+    w, h = font.getsize(message)
+    x = (inkyDisplay.WIDTH / 2) - (w / 2)
+    y = (inkyDisplay.HEIGHT / 2) - (h / 2)
+
+    draw.text((x, y), message, inkyDisplay.RED, font)
+    inkyDisplay.set_image(img)
+    inkyDisplay.show()
+    
+    
+    
+def default():
     print("Hello there")
 
 def main() :
-    initialise()
-    default()
+    initialiseDisplay()
+    initialiseImage()
+    initialiseFont()
+#    default()
+    HelloWorld()
 
 if __name__ == '__main__':
     main()
