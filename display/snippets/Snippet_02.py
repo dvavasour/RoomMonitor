e3195160ff56fa396156c0912e59c457e8fe6d8b
Dunstan Vavasour <dunstan@vavasour.org.uk>
2020-05-20 20:40:16 +0100

    Add Snippet_02 where we start drawing stuff

--- display/snippets/Snippet_02.py
+++ display/snippets/Snippet_02.py
@@ -1,3 +1,4 @@
+import time
 from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
@@ -5,6 +6,14 @@ from font_fredoka_one import FredokaOne
 
 # Some constants
 deviceColour = "black"
+
+
+# Frigged values for testing
+temperature = 25.46
+pressure = 1007
+humidity = 40.17
+resistance = 960000
+
 
 
 
@@ -37,12 +46,19 @@ def initialiseFont():
 
 def HelloWorld():
     print("In HelloWorld function")
-    message = "Hello, World!"
+    message = time.strftime("%d %B %-I:%M %p")
     w, h = font.getsize(message)
     x = (inkyDisplay.WIDTH / 2) - (w / 2)
     y = (inkyDisplay.HEIGHT / 2) - (h / 2)
+
+    width = inkyDisplay.WIDTH
+    height = inkyDisplay.HEIGHT
+    print("Display Width", width, "Display Height", height, "Text Width", w, "Text Height", h)
     # Basic stuff to position the message in the centre of the screen
 
+    draw.line((69, 36, 69, 81), 1)    # Vertical line in black
+    draw.rectangle ([(10,10), (40,40)], 1) # Rectangle ([(x1, y1), (x2, y2)],colour)
+    draw.rectangle ([(13,13), (37,37)], 0)
     draw.text((x, y), message, inkyDisplay.BLACK, font)
     # I think this is where you set the colour that's being displayed
     
